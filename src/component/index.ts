e6feddd0f2be6f12011afa166223966efd36c5bb
Rumyantsev Artem <rumartcer@gmail.com>
2022-09-23 10:26:24 +0500

    Исправил схематику для работы в Angular 14 и повысил версию

--- src/component/index.ts
+++ src/component/index.ts
@@ -2,6 +2,8 @@ import {basename, extname, normalize, strings} from '@angular-devkit/core';
 import {classify} from '@angular-devkit/core/src/utils/strings';
 import {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
 import generateComponent from '@schematics/angular/component/index';
+import {buildDefaultPath, getWorkspace} from '@schematics/angular/utility/workspace';
+import {parseName} from '@schematics/angular/utility/parse-name';
 import {buildRelativePath, findModuleFromOptions, MODULE_EXT} from '@schematics/angular/utility/find-module';
 import {Schema} from './schema';
 
@@ -18,24 +20,45 @@ export default function(_options: Schema): Rule {
  * @param tree
  * @param _options
  */
-function createModuleExtraOptions(tree: Tree, _options: Schema) {
-  if (_options.skipImport) {
+async function createModuleExtraOptions(tree: Tree, _options: Schema) {
+  // копируем опции, чтобы не засорять ненужными данными, но при этом иметь возможность их дополнять и передавать в служебные методы
+  const options = {..._options}
+
+  if (options.skipImport) {
     return;
   }
 
-  const modulePath = findModuleFromOptions(tree, _options);
+  // код ниже по получению path скопирован из схематики Angular
+  // подозреваю всё потому, что Angular теперь требует обязательное указание проекта, а опцию defaultProject в angular.json упразднили
+  const workspace = await getWorkspace(tree);
+  const project = workspace.projects.get(options.project as string);
 
+  if (!project) {
+    return;
+  }
+
+  if (options.path === undefined) {
+    options.path = buildDefaultPath(project);
+  }
+
+  const modulePath = findModuleFromOptions(tree, options);
+  
   if (modulePath) {
-    // таким же образом строятся пути в @schematics/angular, поэтому не удивляйтесь
+    // эта строчка взята из @schematics/angular:component в Angular, для подготовки к поиску подходящего модуля
+    const parsedPath = parseName(options.path as string, options.name);
+
+    // таким же образом строятся пути в @schematics/angular, в функции buildRelativeModulePath (которая не экспортирована, поэтому нельзя её использовать тут)
     const specFilePath = normalize(
-      `/${_options.path}/` +
-      (_options.flat ? '' : strings.dasherize(_options.name) + '/') +
-      strings.dasherize(_options.name) +
+      `/${parsedPath.path}/` +
+      (options.flat ? '' : strings.dasherize(parsedPath.name) + '/') +
+      strings.dasherize(parsedPath.name) +
       '.spec.ts',
     );
 
+    // дополняем исходные опции необходимыми параметрами
     const moduleFileName = basename(modulePath).split(MODULE_EXT)[0];
     _options.moduleName = classify(`${moduleFileName}Module`);
+    // то же самое делается в функции buildRelativeModulePath в схематике @schematics/angular, но я ещё убираю расширение у файла модуля
     _options.moduleRelativePath = buildRelativePath(specFilePath, modulePath).split(extname(modulePath))[0];
   }
 }
